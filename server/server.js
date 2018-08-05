2702be9d2e1314a5ad37b97f74984574fe7047d3
ntn641998 <ntn641998@gmail.com>
2018-08-05 20:41:00 +0700

    Add on Heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {ObjectID} = require('mongodb');
+
 var app = express();
+const   port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=>{
@@ -48,8 +50,8 @@ app.get('/todos/:id', (req,res)=>{
 
 });
 
-app.listen(3000, ()=>{
-    console.log('Started on port 3000');
+app.listen(port, ()=>{
+    console.log('Started on port ',port);
 });
 
 module.exports = {app,Todo};
